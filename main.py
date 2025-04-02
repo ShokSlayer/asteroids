52873e2ac3bb29974e4d96545ce47576fca127ee
shok <bartos.michal141@seznam.cz>
2025-04-02 10:39:24 +0200

    circle

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	pygame.display.set_caption("Asteroids Game")
-
+	pygame.time.Clock()
+	dt = 0
 	running = True
 	while running:
 		for event in pygame.event.get():
@@ -16,7 +17,8 @@ def main():
 				running= False
 		screen.fill((0, 0, 0))
 		pygame.display.flip()
-
+	pygame.time.Clock.tick(60)
+	dt = pygame.time.Clock.tick(60) / 1000
 	pygame.quit()
 
 if __name__ == "__main__":
